d0eb7846affa1ccbb08d46c5518e90743637285f
HTTzero <56182928+HuangTTzero@users.noreply.github.com>
2026-03-24 19:16:58 +0800

    Refactor FASTA processing to use chromosome names
    Updated sequence record IDs and descriptions to use chromosome names extracted from the original descriptions. Changed output file path for the modified FASTA file.

--- snp_masked_reference/fa_name.py
+++ snp_masked_reference/fa_name.py
@@ -2,11 +2,12 @@ import sys
 from Bio import SeqIO
 fasta=[]
 with open(sys.argv[1], "r") as handle:
-    i=0
     for seq_record in SeqIO.parse(handle, "fasta"):
-        i+=1
-        seq_record.id="chr"+i
-        fasta.append(seq_record) 
-#outfile='/data/R02/huangtt39/ATAC-RNAseq/snp/rf/mm10_modified.fa'
-outfile='/data/R02/huangtt39/ATAC-RNAseq/snp/rf/chr_fa/mm10_chr.fa'
+        description=seq_record.description
+        chrom=description.split(' ')[1]
+        chrom=chrom.split(':')[0]
+        seq_record.id=chrom
+        seq_record.description = seq_record.id
+        fasta.append(seq_record)
+outfile='/data/R02/huangtt39/F1_OSN/mapping/reference/rf/mm10-masked-modified.fa'
 SeqIO.write(fasta, outfile, "fasta")
